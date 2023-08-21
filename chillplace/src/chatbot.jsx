05c1bf2802e88c507bc19249d5ded9203ecf6243
Qtruongk <qtruong2109.itech@gmail.com>
2023-08-21 17:44:23 +0700

    change API key

--- chillplace/src/chatbot.jsx
+++ chillplace/src/chatbot.jsx
@@ -4,7 +4,7 @@ import './chatbot.css';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
